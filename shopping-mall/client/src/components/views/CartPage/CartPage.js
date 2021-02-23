b4aa266fc2fe18bfddb83466074ba1ee89f3f962
jeohyn <chajhyeon@gmail.com>
2021-02-23 18:03:29 +0900

    edit details-cart Icon, default cartpage

--- shopping-mall/client/src/components/views/CartPage/CartPage.js
+++ shopping-mall/client/src/components/views/CartPage/CartPage.js
@@ -2,55 +2,76 @@ import React, {useEffect, useState} from 'react'
 import {useDispatch} from 'react-redux'
 import {getCartItems, removeCartItem} from '../../../_actions/user_actions'
 import UserCardBlock from './Sections/UserCardBlock'
+import {Empty} from 'antd'
 
 function CartPage(props) {
-    const dispatch=useDispatch();
-
-    const [TotalPrice, setTotalPrice] = useState(0)
+    const dispatch = useDispatch();
+    const [Total, setTotal] = useState(0)
+    const [ShowTotal, setShowTotal] = useState(false)
 
     useEffect(() => {
 
-        let cartItems=[]
-
+        let cartItems = [];
         //리덕스 user state의 cart안에 상품이 존재하는지 확인
-        if(props.user.userData && props.user.userData.cart){
-            if(props.user.userData.cart.length>0){
-                props.user.userData.cart.forEach(item=>{
+        if (props.user.userData && props.user.userData.cart) {
+            if (props.user.userData.cart.length > 0) {
+                props.user.userData.cart.forEach(item => {
                     cartItems.push(item.id)
-                })
-
+                });
                 dispatch(getCartItems(cartItems, props.user.userData.cart))
-                .then(response=>{calculateTotal(response.payload)})
+                    .then((response) => {
+                        if (response.payload.length > 0) {
+                            calculateTotal(response.payload)
+                        }
+                    })
             }
         }
+
     }, [props.user.userData])
 
-    let calculateTotal=(cartDetail)=>{
-        let total=0;
-        cartDetail.map(item=>{
-            total+=parseInt(item.price, 10)*item.quantity
-        })
-        setTotalPrice(total)
+    const calculateTotal = (cartDetail) => {
+        let total = 0;
+
+        cartDetail.map(item => {
+            total += parseInt(item.price, 10) * item.quantity
+        });
+
+        setTotal(total)
+        setShowTotal(true)
     }
 
-    let removeFromCart=(productId)=>{
+
+    let removeFromCart = (productId) => {
+
         dispatch(removeCartItem(productId))
-        .then(response=>{
-
-        })
+            .then(response => {
+                if (response.payload.cartDetail==undefined || response.payload.cartDetail.length<=0) {
+                    setShowTotal(false)
+                } else {
+                    calculateTotal(response.payload.cartDetail)
+                }
+            })
     }
+
 
     return (
-        <div style={{width:'85%', margin:'3rem auto'}}>
-            <h1> My Cart </h1>
-            <div>
-                <UserCardBlock products={props.user.cartDetail} removeItem={removeFromCart}/>
-            </div>
-            <div style={{marginTop:'3rem'}}>
-                <h2>Total Amount : {TotalPrice}KRW</h2>
-            </div>
+        <div style={{ width: '85%', margin: '3rem auto' }}>
+            <h1>My Cart</h1>
+            {ShowTotal ?
+                <div>
+                    <UserCardBlock
+                    products={props.user.cartDetail}
+                    removeItem={removeFromCart}/>
+                    <h2>Total amount: ${Total} </h2>
+                </div>
+                    :
+                    <Empty description={false}/>
+                }
+                
+            {/* Paypal Button */}
+
+
         </div>
     )
 }
-
 export default CartPage
--- shopping-mall/client/src/components/views/NavBar/Sections/RightMenu.js
+++ shopping-mall/client/src/components/views/NavBar/Sections/RightMenu.js
@@ -37,7 +37,7 @@ function RightMenu(props) {
           <a href="/product/upload">Upload</a>
         </Menu.Item>
         <Menu.Item key="cart" style={{paddingBottom:2}}>
-          <Badge count={5}>
+          <Badge count={user.userData && user.userData.cart.length}>
             <a href="/user/cart" className="head-example" style={{marginRight:-15, color:'#66777'}}>
               <Icon type="shopping-cart" style={{fontSize:30, marginBottom:2}}/>
             </a>
