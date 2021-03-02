b0f3fa68b55e9a623ef52cceb5595d322a3ea7b0
jeohyn <chajhyeon@gmail.com>
2021-03-02 17:25:32 +0900

    add unit of money on product detail page

--- shopping-mall/client/src/components/views/DetailProductPage/Sections/ProductInfo.js
+++ shopping-mall/client/src/components/views/DetailProductPage/Sections/ProductInfo.js
@@ -23,7 +23,7 @@ function ProductInfo(props) {
     return (
         <div>
             <Descriptions title="Product Info" bordered>
-                <Descriptions.Item label="Price">{props.detail.price}</Descriptions.Item>
+                <Descriptions.Item label="Price">${props.detail.price}</Descriptions.Item>
                 <Descriptions.Item label="Sold">{props.detail.sold}</Descriptions.Item>
                 <Descriptions.Item label="View">{props.detail.views}</Descriptions.Item>
                 <Descriptions.Item label="Description">{props.detail.description}</Descriptions.Item>
